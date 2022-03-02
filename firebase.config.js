5fd21fc9e061811c2c3a6cd5023fb9579e751ecc
Vetrivel Ravi <vetrivel.galaxy@gmail.com>
2022-03-02 03:09:28 +0300

    Changes : Updated Local environment file

--- firebase.config.js
+++ firebase.config.js
@@ -3,12 +3,14 @@ import { getFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDiXHHVRJgSd5YDhaiYUaubJspdU-KlLIQ',
-  authDomain: 'nextauthjs-yt.firebaseapp.com',
-  projectId: 'nextauthjs-yt',
-  storageBucket: 'nextauthjs-yt.appspot.com',
-  messagingSenderId: '1046029306672',
-  appId: '1:1046029306672:web:642e4d68de8ffd513a56f5',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHA_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSEAGING_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASURMENT_ID,
 }
 
 const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig)
